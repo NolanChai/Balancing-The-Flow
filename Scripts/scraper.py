e6f464c6b5c8f27f2c10b5ecebe8e07655a3cafd
Nolan Chai <nochai@ucsd.edu>
2025-03-06 14:15:57 -0800

    updated packages, readme for scripts

--- Scripts/scraper.py
+++ Scripts/scraper.py
@@ -5,13 +5,13 @@ import time
 import random
 import requests
 import csv
+import scrapy # may or may not try with this
+import pandas
 from urllib.parse import urljoin, urlparse
 
+
 START_URLS = [
-    "https://www.bbc.com/",
-    "https://edition.cnn.com/",
-    "https://www.reuters.com/",
-    "https://apnews.com/",
+    "https://lite.cnn.com"
 ]
 
 MAX_ARTICLES = 300 # temporary limit
@@ -23,3 +23,15 @@ def is_valid(url):
     valid_scheme = bool(parsed.scheme)
     return valid_connect and valid_scheme
 
+# PSEUDOCODE
+# for each url we have, locate subdomains and articles
+# 
+
+for url in START_URLS:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    # print(response.status_code) # 200 = valid
+    # print(response.text)
+    print(soup.find('title').get_text())
+
+# fetch all articles, go to next page
