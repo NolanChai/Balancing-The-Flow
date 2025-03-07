17d6c896f4b05d1e39e76df53e0aa7aec015cf95
Nolan Chai <nochai@ucsd.edu>
2025-03-06 22:57:25 -0800

    scrape all articles

--- Scripts/scraper.py
+++ Scripts/scraper.py
@@ -7,6 +7,7 @@ import requests
 import csv
 import scrapy # may or may not try with this
 import pandas
+import json
 from urllib.parse import urljoin, urlparse
 
 
@@ -27,13 +28,20 @@ def is_valid(url):
 # for each url we have, locate subdomains and articles
 # 
 
+articles = []
+
 for url in START_URLS:
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
     # print(response.status_code) # 200 = valid
     # print(response.text)
     # print(soup.find('title').get_text())
-    for link in soup.find_all('a')[:-5]:
-        print(link.get('href'))
+    for subdir in soup.find_all('a')[:-5]:
+        articles.append(url + subdir.get('href'))
+    article_dumps = json.dumps(articles)
+
+out = open("articles.json", "w")
+out.write(article_dumps)
+out.close()
 
 # fetch all articles, go to next page
