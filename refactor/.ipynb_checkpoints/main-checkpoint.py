d176e385689de1ab4e2bfa68e8be912fdade9e55
Nolan Chai <nochai@ucsd.edu>
2025-04-10 11:55:50 -0700

    feat: LoRA notebook

---
+++ refactor/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,15 @@
+"""
+Last updated: Apr 10, 2025
+By: Nolan
+
+This is an overhaul of the existing scripting system. 
+The previous version was a bit too convoluted, and not consolidated.
+"""
+from log import run_log
+
+def main():
+    # auto log
+    run_log()
+
+if __name__ == "__main__":
+    main()
